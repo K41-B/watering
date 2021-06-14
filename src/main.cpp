ab20482ed52c583fc458563b0d2b10ab4ffdc65a
Kai Behrendt <behrendt.kai@icloud.com>
2021-06-14 11:41:42 +0200

    _

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 
-//Hysterese und soilshould[] müssen an Werte für Boden und Eigenschaften der Sensoren angepasst werden!
+//globale Variablen
+int soilmid[8];
+int soilshould[8];
+//Schleifenzähler der Bewässerung
+int x=0;
+
+//Hysterese muss an Werte für Boden und Eigenschaften der Sensoren angepasst werden!
 
 
 //INPUTS für Sensoren Stiftleiste
@@ -30,7 +36,7 @@
 #define MW 10
 
 //Hysterese für Überfeuchtung
-#define HYSTERESE 400
+#define HYSTERESE 200
 
 
 //Pumpenstatus umschalten
@@ -61,7 +67,7 @@ void failure()
       while(1)
       {sleep(100000000);}
 }
-//Funktion für Mittelwert aus 25 Messwerten für Bodenfeuchte
+//Funktion für Mittelwert aus MW Messwerten für Bodenfeuchte
 double mittelwert(int * soil)
 {
 int sum=0, i;
@@ -178,10 +184,70 @@ int getsensor7()
  return mittelwert(soil);
 }
 
+
+//Ausgabe der Feuchtigkeitswerte pro Sensor
+void moisturecal()
+{
+  int i;
+  for(i=0;i<8;i++)
+    printf("Sensor:%d | Feuchtigkeitswert:%d\n",i,soilshould[i]);
+
+}
+
+//Ausgabe der Feuchtigkeitswerte pro Sensor mit Soll
+void moisturesens()
+{
+  int i;
+  for(i=0;i<8;i++)
+    printf("Sensor:%d | Sollwert:%d | Feuchtigkeitswert:%d\n" ,i,soilshould[i],soilmid[i]);
+
+}
+
+//Testabschnitt
+void test()
+{
+//Funktion der Pumpe gegeben?
+  printf("Starte Funktionstest der Wasserversorgung\n");
+
+  printf("Starte Pumpe\n");
+  activatepump();
+  sleep(60);
+  
+  printf("Öffne Ventil 1\n");
+  digitalWrite(vent0,HIGH);
+  sleep(60);
+  printf("Schließe Ventil 1\n");
+  digitalWrite(vent0,LOW);
+  sleep(10);
+
+  printf("Öffne Ventil 2\n");
+  digitalWrite(vent1,HIGH);
+  sleep(60);
+  printf("Schließe Ventil 2\n");
+  digitalWrite(vent1,LOW);
+  sleep(10);
+
+  printf("Öffne Ventil 3\n");
+  digitalWrite(vent2,HIGH);
+  sleep(60);
+  printf("Schließe Ventil 3\n");
+  digitalWrite(vent2,LOW);
+  sleep(10);
+
+  printf("Öffne Ventil 4\n");
+  digitalWrite(vent3,HIGH);
+  sleep(60);
+  printf("Schließe Ventil 4\n");
+  digitalWrite(vent3,LOW);
+  sleep(10);
+
+  printf("Deaktiviere Pumpe und alle Ventile\n");
+  deactivateall();
+  }
+
 //SETUP
 void setup() 
 {
-
   //Start Serial
   Serial.begin(115200);
 
@@ -189,42 +255,51 @@ void setup()
   analogReadResolution(12);
 
   //Sensoren sind INPUTS
- pinMode(in0,INPUT);
- pinMode(in1,INPUT);
- pinMode(in2,INPUT);
- pinMode(in3,INPUT);
- pinMode(in4,INPUT);
- pinMode(in5,INPUT);
- pinMode(in6,INPUT);
- pinMode(in7,INPUT);
+  pinMode(in0,INPUT);
+  pinMode(in1,INPUT);
+  pinMode(in2,INPUT);
+  pinMode(in3,INPUT);
+  pinMode(in4,INPUT);
+  pinMode(in5,INPUT);
+  pinMode(in6,INPUT);
+  pinMode(in7,INPUT);
  
  // Pumpe und Ventile sind OUTPUTS
- pinMode(pump0,OUTPUT);
- pinMode(vent0,OUTPUT);
- pinMode(vent1,OUTPUT);
- pinMode(vent2,OUTPUT);
- pinMode(vent3,OUTPUT);
+  pinMode(pump0,OUTPUT);
+  pinMode(vent0,OUTPUT);
+  pinMode(vent1,OUTPUT);
+  pinMode(vent2,OUTPUT);
+  pinMode(vent3,OUTPUT);
 
-  //Rückstellung Ventile aus
-  digitalWrite(vent0,LOW);
-  digitalWrite(vent1,LOW);
-  digitalWrite(vent2,LOW);
-  digitalWrite(vent3,LOW);
+  //Rückstellung Ventile und Pumpe
+  deactivateall();
 
-  //Rückstellung Pumpe aus
-  deactivatepump();
+  //Kalibrierung der Feuchtigkeit auf Einschaltzustand
+  soilshould[0]=getsensor0();
+  soilshould[1]=getsensor1();
+  soilshould[2]=getsensor2();
+  soilshould[3]=getsensor3();
+  soilshould[4]=getsensor4();
+  soilshould[5]=getsensor5();
+  soilshould[6]=getsensor6();
+  soilshould[7]=getsensor7();
 
-//Warte 1min
+  //Ausgabe der Kalibrierten Werte
+  moisturecal();
+
+  //Teste auf Funktion der Wasserversorgung
+  test();
+
+  //Warte 1min
   sleep(60);
-}
-
+  }
 
 
 //LOOP-Bewässerung
 void loop() 
 {
-int soilmid[8];
-int soilshould[8]={2200,2200,2200,2200,2200,2200,2200,2200};
+x=x+1;
+printf("Bewässerungsschleife Durchlauf:%d\n", x);
 
 //Prüfung der Werte
 //Abfragedauer ca. 20 sec pro Sensor. -> ca. 2min 40 sec. bei 8 Sensoren.
@@ -239,11 +314,15 @@ soilmid[6]=getsensor6();
 soilmid[7]=getsensor7();
 }
 
+//Ausgabe der Feuchtigkeit
+moisturesens();
+
 //Bewässern, wenn nötig 
 int k=0;
 //VENTIL 0
 if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
 {
+  printf("Bewässerung für %d nötig",k);
   activatepump();
   delay(2000);
   digitalWrite(vent0,HIGH);
@@ -264,7 +343,16 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
     soilmid[k]=getsensor0();
     soilmid[k+1]=getsensor1();
       if((soilmid[k] > (soilshould[k]-HYSTERESE)) || (soilmid[k+1] > (soilshould[k+1]-HYSTERESE) ))
-        sleep(120);
+        {
+          sleep(120);
+          int help1, help2;
+          help1 = getsensor0();
+          help2 = getsensor1();
+          if (soilmid[k]-10<= help1 || soilmid[k+1]-10<=help2)
+            {       
+              failure();
+            }
+        }
       else
         {
         go=5;
@@ -273,10 +361,13 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
     }
 }
 
+/*
+
 //VENTIL 1
 k=2;
 if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
 {
+  printf("Bewässerung für %d nötig",k);
   activatepump();
   delay(2000);
   digitalWrite(vent1,HIGH);
@@ -297,7 +388,16 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
     soilmid[k]=getsensor2();
     soilmid[k+1]=getsensor3();
       if((soilmid[k] > (soilshould[k]-HYSTERESE)) || (soilmid[k+1] > (soilshould[k+1]-HYSTERESE) ))
+      {
         sleep(120);
+        int help1, help2;
+        help1 = getsensor2();
+        help2 = getsensor3();
+        if (soilmid[k]-10<= help1 || soilmid[k+1]-10<=help2)
+          {
+             failure();
+          }
+      }  
       else
         {
         go=5;
@@ -310,6 +410,7 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
 k=4;
 if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
 {
+  printf("Bewässerung für %d nötig",k);
   activatepump();
   delay(2000);
   digitalWrite(vent2,HIGH);
@@ -330,7 +431,17 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
     soilmid[k]=getsensor4();
     soilmid[k+1]=getsensor5();
       if((soilmid[k] > (soilshould[k]-HYSTERESE)) || (soilmid[k+1] > (soilshould[k+1]-HYSTERESE) ))
-        sleep(120);
+        {
+            sleep(120);
+          //Prüfen ob Bewässerung die Feuchtigkeit erhöht. Wenn nicht failure() auslösen!
+             int help1, help2;
+             help1 = getsensor4();
+             help2 = getsensor5();
+              if (soilmid[k]-10<= help1 || soilmid[k+1]-10<=help2)
+              { 
+                failure();
+              }
+        }
       else
         {
         go=5;
@@ -343,6 +454,7 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
 k=6;
 if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
 {
+  printf("Bewässerung für %d nötig",k);
   activatepump();
   delay(2000);
   digitalWrite(vent3,HIGH);
@@ -363,17 +475,31 @@ if((soilmid[k] > soilshould[k]) || (soilmid[k+1] > soilshould[k+1]))
     soilmid[k]=getsensor6();
     soilmid[k+1]=getsensor7();
       if((soilmid[k] > (soilshould[k]-HYSTERESE)) || (soilmid[k+1] > (soilshould[k+1]-HYSTERESE) ))
-        sleep(120);
+       {
+          sleep(120);
+        //Prüfen ob Bewässerung die Feuchtigkeit erhöht. Wenn nicht failure() auslösen!
+          int help1, help2;
+          help1 = getsensor6();
+          help2 = getsensor7();
+            if (soilmid[k]-10<= help1 || soilmid[k+1]-10<=help2)
+            {
+              failure();
+            } 
+        }
       else
         {
         go=5;
         deactivateall();
         }
     }
-} 
-  
+}
+
+  */
+
+
   //Rückstellung der Werte
   deactivateall();
+  printf("Bewässerungszyklus %d abgeschlossen", x);
 
   //Schlafe 1h
   sleep(3600);
